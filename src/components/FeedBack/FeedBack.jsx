352d69dbacd83f4370c0471365c71b7fe95eae87
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2023-07-31 18:13:29 +0300

    create page maket

---
+++ src/components/FeedBack/FeedBack.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FeedBack = () => <div>
+    <h2>Please leave feedback</h2>
+    <div>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+    </div>
+</div>
+
+
+export default FeedBack;
